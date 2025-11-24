1902fa938d2c9dc8d04a52c7576c7a6b88daeaa1
syed mujtaba ali <nushat786syed@gmail.com>
2025-11-24 23:31:07 +0500

    Add files via upload

---
+++ venv/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+with open('housppp_price_prediction.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        features = [
+            float(request.form['CRIM']),
+            float(request.form['ZN']),
+            float(request.form['INDUS']),
+            float(request.form['CHAS']),
+            float(request.form['NOX']),
+            float(request.form['RM']),
+            float(request.form['AGE']),
+            float(request.form['DIS']),
+            float(request.form['RAD']),
+            float(request.form['TAX']),
+            float(request.form['PTRATIO']),
+            float(request.form['B']),
+            float(request.form['LSTAT'])
+        ]
+
+        features_array = np.array([features])
+
+        prediction = model.predict(features_array)
+        output = round(prediction[0], 2)
+
+        return render_template('index.html', prediction_text=f"Predicted Price: ${output}")
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
